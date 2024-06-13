22881e09a323e44356f4577577893e1c4ffefcec
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-13 10:04:14 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [values, setValues] = useState({
+    country: '', language: '', accept: false, select: false
+  })
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log('sending data...', values) // here I can send data to server
+  }
+
+  const handleChange = (evt) => {
+    const { name, value, type, checked } = evt.target
+    setValues({
+      ...values,
+      [name]: type === 'checkbox' ? checked : value
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Dropdowns, Checkboxes and Radiobuttons</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Country <select name="country" onChange={handleChange} value={values.country}>
+            <option>*Select Country*</option>
+            <option value="Hu" >Hungary</option>
+            <option value="US" >U.S.A.</option>
+            <option value="ES" >Spain</option>
+          </select>
+        </label>
+        <div className="radio-container">
+          <label>
+            <input
+              type="radio"
+              name="language"
+              value="javascript"
+              onChange={handleChange}
+              checked={values.language === 'javascript'}
+            /> Javascript
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="language"
+              value="kotlin"
+              onChange={handleChange}
+              checked={values.language === 'kotlin'}
+            /> Kotlin
+          </label>
+        </div>
+        <div className="checkbox-container">
+          <label>
+            <input
+              type="checkbox"
+              name="accept"
+              onChange={handleChange}
+              checked={values.accept}
+            /> Accept terms and conditions
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="select"
+              onChange={handleChange}
+              checked={values.select}
+            /> Send me emails
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+
